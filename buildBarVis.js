97fd3c378f9c488ccea38fcc769f5b70954ee990
Christina Aiello <cjaiello@wpi.edu>
2015-11-26 12:04:42 -0500

    Can now search for a word in all of the questions, answers, and categories

--- buildBarVis.js
+++ buildBarVis.js
@@ -105,3 +105,121 @@ function buildBarVis(data, translateXCoordinate, vizNumber, vizLabel) {
       .text(vizLabel);
 
 }
+
+// Creates a bar visualization
+// @data: the data from the csv
+// @translateXCoordinate: How far over (x-wise) the graph should go
+// @vizNumber: The number of the viz, either 1 2 or 3
+// @vizLabel: The label for the viz (what attribute it is)
+function buildWordMatchBarVis(dataObject, translateXCoordinate, vizNumber, vizLabel) {
+
+  d3.select(".countWordsVis")
+        .remove();
+
+  console.log("Trying to make graph for word");
+
+  // Make an object for each attribute:
+  var questionData = new Object();
+  questionData.Attribute = "Question";
+  questionData.Value = dataObject.Questions;
+  var answerData = new Object();
+  answerData.Attribute = "Answer";
+  answerData.Value = dataObject.Answers;
+  var categoryData = new Object();
+  categoryData.Attribute = "Category";
+  categoryData.Value = dataObject.Categories;
+
+  // Creating a list of this data:
+  var data = [];
+  data.push(questionData);
+  data.push(answerData);
+  data.push(categoryData);
+
+  console.log(data);
+
+  var w = 700;
+  var h = 700;
+
+  var format = d3.format(",.0f");
+
+  var x = d3.scale.linear().range([10, w-50]),
+      y = d3.scale.ordinal().rangeBands([0, h-50], .1);
+
+  var xAxis = d3.svg.axis().scale(x).orient("top").ticks(4).tickSize(-h + 60),
+      yAxis = d3.svg.axis().scale(y).orient("left").tickSize(0);
+
+  var svg = d3.select("#locationOfSVGs").append("svg")
+      .attr("class", "countWordsVis")
+      .attr("padding-left", "20")
+      .attr("margin-left", "20")
+      .attr("width", w+125)
+      .attr("height", h)
+    .append("g")
+      .attr("transform", "translate(" + (translateXCoordinate + 25) + "," + 30 + ")");
+
+  // Set the scale domain.
+  x.domain([0, d3.max(data, function(d) {
+    console.log("X Domain. D is:");
+    console.log(d);
+    console.log("X Domain. D.Value is:");
+    console.log(d.Value);
+    return d.Value; 
+  })]);
+  y.domain(data.map(function(d) { return d.Attribute; }));
+
+  // Adding x axis to screen
+  svg.append("g")
+      .attr("class", "x axis")
+      .call(xAxis);
+
+  // Adding y axis to screen
+  svg.append("g")
+      .attr("class", "labels")
+      .attr("transform", "translate(" + 10 + ",0)")
+      .call(yAxis);
+
+  var bar = svg.selectAll("g.bar")
+      .data(data)
+    .enter().append("g")
+      .attr("class", "bar")
+      .attr("transform", function(d) {
+        return "translate(1," + y(d.Attribute) + ")"; 
+      });
+
+  // Putting the rectangles on the bar chart
+  bar.append("rect")
+      .attr("fill", "#399DB1")
+      .attr("transform", "translate(" + 10 + ",0)")
+      .attr("width", function(d) {
+        console.log(d);
+        console.log(d.Value);
+        console.log(parseInt(d.Value));
+        console.log(x(parseInt(d.Value)));
+        return x(parseInt(d.Value)); 
+      })
+      .attr("height", y.rangeBand());
+
+  // Placing the label text for each bar
+  bar.append("text")
+      .attr("class", "labels")
+      .attr("x", function(d) { 
+        return x(parseInt(d.Value)); 
+      })
+      .attr("y", y.rangeBand() / 2)
+      .attr("dx", 20)
+      .attr("dy", ".35em")
+      .style("font-weight","bold")
+      .attr("text-anchor", "end")
+      .text(function(d) {
+        return d.Value; 
+      });
+
+  svg.append("text")
+      .attr("x", w / 2 - 50 )
+      .attr("y", h - 30)
+      .style("font-size","15px")
+      .style("font-weight","bold")
+      .attr("fill", "black")
+      .text(vizLabel);
+
+}
--- scripts.js
+++ scripts.js
@@ -1,13 +1,3 @@
-d3.csv('data/jeopardy_questions_and_answers_preprocessed_groupingvalues.csv', function(err, data) {
-  if(err) console.log(err);
-
-  // Aggregate data set
-  data = aggregateByRound(data);
-
-  //buildBarVis(data, 85, 1, "Round");
-  buildBarVis(data, 85, 1, "Round");
-});
-
 /*
 * Helper for aggregating data
 * @data: The data from the csv to aggregate
@@ -70,6 +60,39 @@ function getVizName(vizNumber){
   } else return "fourthChart";
 }
 
+// This prevents the user from leaving the box blank
+// and prevents them from entering more than one word
+function validateForm() {
+  var usersWord = document.getElementById("wordToSearchFor").value;
+  if (usersWord==null || usersWord== "" || usersWord.contains(" ")) {
+    alert("Please enter a singular word to search for.");
+    return false;
+  } else return true;
+}
+
+// Function to find the number of word matches in the data
+// set. Finds number of matches in questions, answers, and categories
+function findWordMatchesInDataSet(){
+  validateForm();
+  
+  console.log("Starting now...");
+  // First, load the data set
+  d3.csv('data/jeopardy_questions_and_answers_preprocessed_groupingvalues.csv', function(err, data) {
+    if(err) console.log(err);
+
+    // Next, get the word we need from the screen
+    var wordToSearchFor = document.getElementById('wordToSearchFor').value;
+    console.log("Searching for: " + wordToSearchFor);
+    // Array with number of matches
+    var numberOfMatchesFound = findMatches(data, wordToSearchFor);
+    console.log(numberOfMatchesFound);
+    console.log("Matches found. Question: " + numberOfMatchesFound.Questions + " | Answer: " + numberOfMatchesFound.Answers + " | Category: " + numberOfMatchesFound.Categories);
+
+    // Now, build the visualization based on the data:
+    buildWordMatchBarVis(numberOfMatchesFound, 50, 1, "Appearances of the Word \"" + wordToSearchFor + "\"");
+  });
+}
+
 
 
 // This function will find matches for a specific word in a given
@@ -77,23 +100,65 @@ function getVizName(vizNumber){
 // @input: An array of strings
 // @ wordToFind: The word you want to find in the given input
 function findMatches(input, wordToFind){
-  var numberOfMatches = 0; 
-  console.log(input);
-  // Go through all questions in this piece of data:
+  // Holds number of matches in entire data set:
+  var numberOfQuestionMatches = 0; 
+  var numberOfAnswerMatches = 0; 
+  var numberOfCategoryMatches = 0; 
+
+  // Loop through the data set:
   for(var counter = 0; counter < input.length; counter++){
-    var currentQuestion = input[counter];
+    var dataPoint = input[counter];
+    var currentQuestion = dataPoint.Question;
+    var currentAnswer = dataPoint.Answer;
+    var currentCategory = dataPoint.Category;
+
+    // Remove commas and parentheses
     currentQuestion = currentQuestion.replace(/,/, "");
     currentQuestion = currentQuestion.replace(/\)/, "");
     currentQuestion = currentQuestion.replace(/\(/, "");
     currentQuestion = currentQuestion.replace(/\""/, "");
-    // And find how many matches for a particular word there are:
+    currentAnswer = currentAnswer.replace(/,/, "");
+    currentAnswer = currentAnswer.replace(/\)/, "");
+    currentAnswer = currentAnswer.replace(/\(/, "");
+    currentAnswer = currentAnswer.replace(/\""/, "");
+    currentCategory = currentCategory.replace(/,/, "");
+    currentCategory = currentCategory.replace(/\)/, "");
+    currentCategory = currentCategory.replace(/\(/, "");
+    currentCategory = currentCategory.replace(/\""/, "");
+
+    // Regex to find how many matches for a particular word there are:
     var regex = new RegExp("(?:^|\\s)" + wordToFind + "(?=\\s|$)");
+
+    // Compare this word to the current question
     var matches = currentQuestion.toLowerCase().match(regex);
     if(matches != null){
-      numberOfMatches += matches.length;
+      numberOfQuestionMatches += matches.length;
     }
+
+    // Compare this word to the current answer
+    var matches = currentAnswer.toLowerCase().match(regex);
+    if(matches != null){
+      numberOfAnswerMatches += matches.length;
+    }
+
+    // Compare this word to the current category
+    var matches = currentCategory.toLowerCase().match(regex);
+    if(matches != null){
+      numberOfCategoryMatches += matches.length;
+    }
+
+    // Now, for this data point (in regards to the question, answer, and category)
+    // we have the number of matches in numberOfMatches. Continue looping through
+    // the entire data set, and return the final counts.
+
   }
-  return numberOfMatches;
+
+  // This will hold the number of matches for each attribute
+  var returnedArrayOfMatches = {};
+  returnedArrayOfMatches["Questions"] = numberOfQuestionMatches;
+  returnedArrayOfMatches["Answers"] = numberOfAnswerMatches;
+  returnedArrayOfMatches["Categories"] = numberOfCategoryMatches;
+  return returnedArrayOfMatches;
 }
 
 
