a1730ad579b8421730a2485905ea995183026bb1
Christina Aiello <caiello@datto.com>
2020-09-22 12:52:53 -0400

    bringing some of my stuff back

--- public/scripts/barVisualizations.js
+++ public/scripts/barVisualizations.js
@@ -241,7 +241,7 @@ var yAxis = d3.svg.axis()
 
   svg.append("text")
       .attr("x", w * (1/20))
-      .attr("y", h)
+      .attr("y", h-50)
       .style("font-size","18px")
       .style("font-weight","bold")
       .attr("fill", "#0B486B")
