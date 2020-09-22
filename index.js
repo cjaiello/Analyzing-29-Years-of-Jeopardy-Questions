054f872828a7bb7df2f800228cf708e97bf3234f
Christina Aiello <caiello@datto.com>
2020-09-22 09:45:46 -0400

    bringing some of my stuff back

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
+var app = express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+var router = express.Router();
+
+router.get('/dataBreakdown', function(req, res, next) {
+        res.render('./views/dataBreakdown');
+});
+
+router.get('/compareWordFrequencies', function(req, res, next) {
+        res.render('./views/compareWordFrequencies');
+});
